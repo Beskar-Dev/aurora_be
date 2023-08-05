9fe1b9c61f5a767b6fc3038009d6f6d7a3ee9a94
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2023-08-05 06:43:45 -0500

    cors policy created

--- Aurora_be/Program.cs
+++ Aurora_be/Program.cs
@@ -3,6 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("https://localhost:3000, https://aurora-front-zeta.vercel.app/")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
